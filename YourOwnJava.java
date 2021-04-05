5a68e9f85def755c0c158476aaf0e7ca6c914967
priyascape <priyascape@gmail.com>
2021-04-05 19:51:10 +0100

    YourOwnJava

---
+++ YourOwnJava.java
@@ -0,0 +1,7 @@
+public class YourOwnJava {
+    public static void main(String[] args){
+        System.out.println("My name is Priya Prakash");
+        System.out.println("I am 45 years old");
+        System.out.println("My hometown is Bombay, India");
+    }
+}
